c9c7d442921ebd1c49fb716c42a8da8de4938102
oliverwalsh7 <oliverwalsh7@gmail.com>
2020-03-28 17:45:02 -0400

    html restructure

---
+++ js/input_form.js
@@ -0,0 +1,15 @@
+// grades
+var grade1, grade2, grade3, grade4, grade5;
+var grades = [grade1, grade2, grade3, grade4, grade5];
+// credits
+var c1, c2, c3, c4, c5;
+var credits = [c1, c2, c3, c4, c5];
+
+function get_grades () {
+
+}
+
+function avg_calc () {
+
+
+}
