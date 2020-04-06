a61510191763946619f09ac8ed50098753362e9d
Maxim Vezenov <mav417@lehigh.edu>
2020-04-06 15:38:41 -0400

    retake boxes working now

--- js/script.js
+++ js/script.js
@@ -7,6 +7,33 @@ import PassFailCalculator from './PassFailCalculator.js';
 const passFailCalcPage = new PassFailCalculator();
 const classGradeCalcPage = new GradeCalculator();
 
+export function isChecked(id) {
+    console.log("id is: " + id);
+    // Get the checkbox
+    var checkBox = document.getElementById(id);
+    // Get the output text
+    var text = document.getElementById("oldGrade-"+ id);
+    var col = document.getElementById("th");
+
+    if (id == undefined) {
+        console.log("it is undefined");
+        //col.style.display = "none";
+        return;
+    }
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        console.log("Checkbox is checked");
+        text.style.display = "block";
+        text.style.position = "inherit";
+        //col.style.display = "block";
+    } else {
+        console.log("Checkbox is unchecked");
+        text.style.display = "none";
+        // /col.style.display = "none";
+    }
+}
+
 $(document).ready(function() {
 
 
@@ -27,9 +54,9 @@ $(document).ready(function() {
         passFailCalcPage.removeRow();
     });
 
-    $('.input-button').click(function() {
-        passFailCalcPage.isChecked($(this).attr('id'));
-    });
+    // $('.input-button').click(function() {
+    //     passFailCalcPage.isChecked($(this).attr('id'));
+    // });
 
     $('#classGradeCalc-page-btn').click(function () {
         passFailCalcPage.hide();
